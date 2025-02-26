2c2789b4ccb1821bcbd41915f32dfa531b89074a
udisha1234 <udisha2704@gmail.com>
2025-02-26 18:37:45 +0530

    feat: connect MongoDB

---
+++ businessai_node_backend/routes/userRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const router = express.Router();
+
+// Sample route
+router.get("/users", (req, res) => {
+  res.json({ message: "List of users" });
+});
+
+export default router; // Use `export default` instead of `module.exports`
